61cb718510320cbe541b128849e6ddbab7112199
maxix28 <maxyxpavliv@gmail.com>
2024-04-13 21:51:23 +0300

    fix tasks location

--- backend/routes/tasksRoute.js
+++ backend/routes/tasksRoute.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router  = express.Router();
 const path = require("path");
 
-const authController = require("../controllers/authController");
+
 const taskController = require("../controllers/taskContrioller");
 
 router.get('/getTasks', taskController.getTasks);
+router.post('/addTask', taskController.createTask);
+router.put('/UpdateTask', taskController.updateTaskProgress);
+
 
 module.exports = router;
