58aebe152eeeee7407235566fe6af7481a7d1dc6
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-04-14 00:28:55 +0300

    fix: added request for create organization

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,14 +1,12 @@
 const express = require("express");
-const router  = express.Router();
+const router = express.Router();
 const path = require("path");
 
 const authController = require("../controllers/authController");
 
-
-
-
-router.post('/signup', authController.createUser);
+router.post("/signup", authController.createUser);
+router.post("/orgSignup", authController.createOrganization);
 // router.post('/addTask', taskController.createTask);
 // router.put('/UpdateTask', taskController.updateTaskProgress);
 
-module.exports = router; 
+module.exports = router;
