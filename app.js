0cf99d95f648b3abbab096b500d4346fd8b0cbd8
Sofia <sofia.pylnyk@gmail.com>
2024-04-13 22:16:47 +0300

    fix: fix conflicting files

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ const cors = require('cors');
 const port = 3001;
 
 const authRouter = require("./backend/routes/authRoute");
+const taskRouter = require("./backend/routes/tasksRoute");
 const organizationRouter = require('./backend/routes/organizationRoute');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-
 app.use("/auth", authRouter);
+app.use("/tasks", taskRouter);
 app.use("/organizations", organizationRouter);
 
 app.listen(port, async () => {
