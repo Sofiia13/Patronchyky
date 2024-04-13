ce6bc112e98a427846b3eaef89d50f4352831673
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-04-14 00:27:11 +0300

    feat: added page for register like organization

--- frontend/src/pages/SignupForUserPage.js
+++ frontend/src/pages/SignupForUserPage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const SignupPage = () => {
+const SignupForUserPage = () => {
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
@@ -47,7 +47,7 @@ const SignupPage = () => {
   return (
     <body>
       <div className="form-area">
-        <h3 className="login-signup-title">Sign Up for organisation</h3>
+        <h3 className="login-signup-title">Sign Up for user</h3>
         <form className="form-content" onSubmit={handleSubmit}>
           <input
             className="input-wrapper"
@@ -79,10 +79,6 @@ const SignupPage = () => {
             onChange={handleChange}
             required
           />
-          <textarea
-            className="org-description"
-            placeholder="Describe your organization"
-          ></textarea>
           <button className="submit-button" type="submit" id="signupButton">
             Sign Up
           </button>
@@ -93,9 +89,9 @@ const SignupPage = () => {
             </a>
           </p>
           <p className="text-link">
-            Do you want to register like ordinary user?{" "}
-            <a href="signupouser" className="link">
-              Sign up for user
+            Do you want to register like organisation?{" "}
+            <a href="signuporgan" className="link">
+              Sign up for organization
             </a>
           </p>
         </form>
@@ -104,4 +100,4 @@ const SignupPage = () => {
   );
 };
 
-export default SignupPage;
+export default SignupForUserPage;
