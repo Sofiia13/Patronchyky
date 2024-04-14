04e0f26d751a89258af2b532c5851d0af9cbede2
9ta1ls <rpeleh19@gmail.com>
2024-04-14 03:28:24 +0300

    Added jwt auth(middlewares) + created login functions

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -4,11 +4,15 @@ const path = require("path");
 
 const authController = require("../controllers/authController");
 
+const {validateTokenForLogged} = require('../JWT/JWT');
 
 
+router.post('/signup', validateTokenForLogged, authController.createUser);
 
-router.post('/signup', authController.createUser);
-// router.post('/addTask', taskController.createTask);
-// router.put('/UpdateTask', taskController.updateTaskProgress);
+router.post('/login-for-user', validateTokenForLogged, authController.loginForUser);
+
+router.post('/login-for-org', validateTokenForLogged, authController.loginForOrg)
+
+router.post("/orgSignup", validateTokenForLogged , authController.createOrganization);
 
 module.exports = router; 
--- backend/routes/tasksRoute.js
+++ backend/routes/tasksRoute.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router  = express.Router();
 const path = require("path");
 
+const {validateTokenForOrg, validateToken} = require('../JWT/JWT');
 
 const taskController = require("../controllers/taskContrioller");
 
-router.get('/getTasks/:id', taskController.getTasks);
-router.post('/addTask', taskController.createTask);
-router.put('/UpdateTask', taskController.updateTaskProgress);
+router.get('/getTasks/:id', validateToken , taskController.getTasks);
+
+router.post('/addTask', validateTokenForOrg , taskController.createTask);
+
+router.put('/UpdateTask', validateTokenForOrg , taskController.updateTaskProgress);
 
 
 module.exports = router;
